f85c9574fb9ba1b0b3368600a7e5957434488fe8
DaRealEndless <76487310+DaRealEndless@users.noreply.github.com>
2021-07-21 10:55:09 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyB4tNOmnCri5LlilBScUTmrf_YADfRQCj0",
+    authDomain: "letschat-9ba3b.firebaseapp.com",
+    projectId: "letschat-9ba3b",
+    storageBucket: "letschat-9ba3b.appspot.com",
+    messagingSenderId: "895103684243",
+    appId: "1:895103684243:web:5928839509d31687d38766",
+    measurementId: "G-V6GWCEC69F"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
